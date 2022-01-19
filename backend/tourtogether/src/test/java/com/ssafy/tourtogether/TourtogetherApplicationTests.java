b67c0e288221a5d62b03a332bcf6652bde14200b
minjunglee <3sally@naver.com>
2022-01-19 13:25:38 +0900

    :memo: skeleton code commit

---
+++ backend/tourtogether/src/test/java/com/ssafy/tourtogether/TourtogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.tourtogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourtogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
