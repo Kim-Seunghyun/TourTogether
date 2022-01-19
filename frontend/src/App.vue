8a25729ad8716f9837f742cedf1669f64bc652cb
Gyum <ksh7669@naver.com>
2022-01-19 11:50:29 +0900

    :sparkles: initial frontend commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="map">
+    <Map />
+  </div>
+</template>
+
+<script>
+import Map from "./map.vue";
+
+export default {
+  name: "App",
+  components: {
+    Map,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#map {
+  width: 100%;
+  /* height: 60%; */
+  height: 1000px;
+}
+</style>
