dd0b629794513613d48bcf47bbe66b9c213ffd04
minjunglee <3sally@naver.com>
2022-02-04 14:45:49 +0900

    Test

--- frontend/src/views/WebRTC.vue
+++ frontend/src/views/WebRTC.vue
@@ -86,7 +86,10 @@ import UserVideo from "@/components/UserVideo";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
 // const OPENVIDU_SERVER_URL = "https://" + location.hostname + ":4443";
-const OPENVIDU_SERVER_URL = "https://i6a105.p.ssafy.io";
+// 서버 배포 시
+const OPENVIDU_SERVER_URL = "https://i6a105.p.ssafy.io:5443";
+// 로컬에서 테스트 시
+// const OPENVIDU_SERVER_URL = "https://localhost:8080";
 const OPENVIDU_SERVER_SECRET = "twist";
 
 export default {
