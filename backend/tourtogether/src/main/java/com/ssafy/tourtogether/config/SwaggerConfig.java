52a3af327647e9cc8d8d62962425f1bad499488d
minjunglee <3sally@naver.com>
2022-02-05 21:04:49 +0900

    TEST

--- backend/tourtogether/src/main/java/com/ssafy/tourtogether/config/SwaggerConfig.java
+++ backend/tourtogether/src/main/java/com/ssafy/tourtogether/config/SwaggerConfig.java
@@ -1,36 +1,22 @@
 package com.ssafy.tourtogether.config;
 
-import static com.google.common.collect.Lists.newArrayList;
-
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger.web.UiConfiguration;
-import springfox.documentation.swagger.web.UiConfigurationBuilder;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * API 문서 관련 swagger2 설정 정의.
  */
 @Configuration
-@EnableSwagger2
 @EnableWebMvc
-public class SwaggerConfig{
+public class SwaggerConfig {
 
 //    @Bean
 //    public Docket api() {
@@ -44,38 +30,18 @@ public class SwaggerConfig{
 //                .apiInfo(apiInfo())
 //                ;
 //    }
-	
-//	@Bean
-//    public Docket api() {
-//        return new Docket(DocumentationType.OAS_30)
-//                .useDefaultResponseMessages(false)
-//                .select()
-//                .apis(RequestHandlerSelectors.basePackage("com.ssafy.tourtogether.api.controller"))
-//                .paths(PathSelectors.any())
-//                .build()
-//                .apiInfo(apiInfo());
-//    }
-	
-	
-	@Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.ssafy.tourtogether.api.controller"))
-                .paths(PathSelectors.any())
-                .build();
-    }
-    
-    private ApiInfo apiInfo() {
-        String description = "TourTogether Project by twist";
-        return new ApiInfoBuilder()
-                .title("TourTogether")
-                .description(description)
-                .version("1.0")
-                .build();
-    }
 
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).useDefaultResponseMessages(false).select()
+				.apis(RequestHandlerSelectors.basePackage("com.ssafy.tourtogether.api.controller"))
+				.paths(PathSelectors.any()).build().apiInfo(apiInfo());
+	}
+
+	private ApiInfo apiInfo() {
+		String description = "Welcome TourTogether";
+		return new ApiInfoBuilder().title("TourTogether").description(description).version("1.0").build();
+	}
 
 //    private ApiKey apiKey() {
 //        return new ApiKey(SECURITY_SCHEMA_NAME, "Authorization", "header");
