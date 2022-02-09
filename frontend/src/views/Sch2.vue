1278dd7a5efc35206af37adc7ace07f8e605916f
zoyul <psa9456@gmail.com>
2022-02-09 22:21:44 +0900

    :ambulance: change redis server url

--- frontend/src/views/Sch2.vue
+++ frontend/src/views/Sch2.vue
@@ -181,7 +181,7 @@ export default {
           userSpotId: 1,
           tourSpotId: 1,
           order: 1,
-          additional: "aaaa"
+          additional: "bbb"
         },
       })
         .then(res => {
