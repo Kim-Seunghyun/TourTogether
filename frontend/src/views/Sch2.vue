4554fa7b4c07fda547224a8565a4a7a9f9846876
zoyul <psa9456@gmail.com>
2022-02-09 11:17:57 +0900

    :sparkles: add redis

--- frontend/src/views/Sch2.vue
+++ frontend/src/views/Sch2.vue
@@ -17,21 +17,24 @@
 
     <button @click="getAllSchedule()">redis에서 값 찾기</button>
 
+    <ul>
+      <li v-for="(sch, index) in scheduleList" :key="index">
+        {{ sch }}
+      </li>
+    </ul>
+
+    <button @click="sendChange()">값 변경 axios 테스트</button>
+
+    <button @click="sendChange()">websocket 보내기</button>
     <hr>
 
-    <div v-for="(item,index) in list" :key="index">
-      <p>
-        {{ item.name }}
-      </p>
-    </div>
     <textarea id="area" cols="30" rows="10">hi</textarea>
-    <button @click="change()">list 순서 바꾸기</button>
 
   </div>
 </template>
 
 <script>
-// import axios from "axios";
+import axios from "axios";
 import SockJS from "sockjs-client";
 
 export default {
@@ -51,7 +54,17 @@ export default {
           id: 1,
           name: "2번",
         }
-      ]
+      ],
+      scheduleList: Object,
+      schdeule: {
+        scheduleId: 1,
+        boardId: 100,
+        day: 1,
+        userSpotId: 1,
+        tourSpotId: 1,
+        order: 1,
+        additional: "변경",
+      }
     }
   },
   computed: {
@@ -68,30 +81,46 @@ export default {
         alert("연결 성공");
         document.getElementById("memoConnect").setAttribute("hidden", "hidden");
       }
-      // this.sockjs.onmessage = function (data) {
-      //   console.log(typeof(data.data));
-      //   console.log("data받음 > " + data.data);
-      //   // this.arr[0] = "데이터받음";
-      // },
-      this.sockjs.addEventListener('message', function (event) {
-        console.log(event.data);
-        console.log("데이터 받고 여기" + this.arr);
-        this.arr[0] = "Z";
-        console.log("데이터 받고 저기" + this.arr);
-        var p = document.getElementById("ptag");
-        p.innerText = (event.data);
-        var area = document.getElementById("area");
-        area.innerText = (event.data);
-      })
+      this.sockjs.onmessage = function (data) {
+        console.log(data);
+        console.log(typeof(data.data));
+        console.log("data받음 > " + data.data);
+        axios({
+          method: 'get',
+          url: 'http://127.0.0.1:8081/redisschedule/all'
+        })
+        .then(res => {
+          console.log(res.data);
+          this.scheduleList = res.data.redisScheduleList;
+          console.log(this.scheduleList);
+        })
+      }
+      // this.sockjs.addEventListener('message', function () {
+      //   axios({
+      //     method: 'get',
+      //     url: 'http://127.0.0.1:8081/redisschedule/all'
+      //   })
+      //     .then(res => {
+      //       console.log("websocket 받은 res : " + res.data.redisScheduleList);
+      //       this.scheduleList = res.data.redisScheduleList;
+      //       console.log("websocket 받은 후 : " + this.scheduleList);
+      //     })
+      //     .catch(err => {
+      //       console.log(err);
+      //   })
+      //   console.log("websocket 받은 후 : " + this.ScheduleList);
+      //   console.log("실행");
+        // console.log("데이터 받고 여기" + this.arr);
+        // this.arr[0] = "Z";
+        // console.log("데이터 받고 저기" + this.arr);
+        // var p = document.getElementById("ptag");
+        // p.innerText = (event.data);
+        // var area = document.getElementById("area");
+        // area.innerText = (event.data);
+      // })
     },
     sendMessage() {
       console.log("sendMessage: ");
-    },
-    change() {
-      this.list[0].id=2;
-      this.list[1].id=1;
-      console.log(this.list);
-      console.log();
     },
     updateArray () {
       this.arr[0] = "Z";
@@ -99,7 +128,44 @@ export default {
       this.sockjs.send(this.arr);
     },
     getAllSchedule() {
-      
+      axios({
+        method: 'get',
+        url: 'http://127.0.0.1:8081/redisschedule/all'
+      })
+        .then(res => {
+          console.log(res.data.redisScheduleList);
+          this.scheduleList = res.data.redisScheduleList;
+          console.log(this.scheduleList);
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    },
+    sendChange: function() {
+      // const schedule = {
+      //   scheduleId: 1,
+      // }
+      // 수정 후 websocket으로 알리기
+      axios({
+        method: 'delete',
+        url: 'http://127.0.0.1:8081/redisschedule/delete',
+        // headers:{
+        //   'Content-Type': 'application/json',
+        //   'Accept' : 'application/json',
+        //   'Access-Control-Allow-Origin' : '*',
+        //   'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+        // },
+        data: {
+          scheduleId: 1,
+        },
+      })
+        .then(res => {
+          console.log(res);
+        })
+        .catch(err => {
+          console.log(err);
+        })
+      // this.sockjs.send("값 변경");
     }
 
   }
