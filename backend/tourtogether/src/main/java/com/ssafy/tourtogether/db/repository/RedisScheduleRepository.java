b9cb2592ecd794709ef35ae0c4058e1f2bb5b20b
zoyul <psa9456@gmail.com>
2022-02-10 10:38:06 +0900

    Revert "Merge branch 'feature/redis-test' into develop"
    This reverts commit 04bb035e89891d92494c7fa71a3abe0d81c40e53, reversing
    changes made to cee5a1c6cd6873ac39c71ffe3531a533e2d51f8e.

--- backend/tourtogether/src/main/java/com/ssafy/tourtogether/db/repository/RedisScheduleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.tourtogether.db.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.ssafy.tourtogether.db.entity.RedisSchedule;
-
-public interface RedisScheduleRepository extends CrudRepository<RedisSchedule, Long>  {
-
-}
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,11 +5,6 @@ import Login from "../views/Login.vue";
 import MyPage from "../views/MyPage.vue";
 import KakaoLoginCallback from "../views/KakaoLoginCallback.vue";
 import FavoritePage from "../views/FavoritePage.vue";
-import Memo from "../views/Memo.vue";
-import Memo2 from "../views/Memo2.vue";
-import Memo3 from "../views/Memo3.vue";
-import Schedule from "../views/Schedule.vue";
-import Sch2 from "../views/Sch2.vue";
 
 const routes = [
   {
@@ -42,31 +37,6 @@ const routes = [
     name: "KakaoLoginCallback",
     component: KakaoLoginCallback,
   },
-  {
-    path: "/memo",
-    name: "Memo",
-    component: Memo,
-  },
-  {
-    path: "/memo2",
-    name: "Memo2",
-    component: Memo2,
-  },
-  {
-    path: "/memo3",
-    name: "Memo3",
-    component: Memo3,
-  },
-  {
-    path: "/schedule",
-    name: "Schedule",
-    component: Schedule,
-  },
-  {
-    path: "/sch2",
-    name: "Sch2",
-    component: Sch2,
-  }
 ];
 
 const router = createRouter({
