d494ddfe3e12130d84ce604252da662b756702c9
prkg7341 <prkg7341@gmail.com>
2022-02-11 18:02:03 +0900

    :ambulance: Fix: fix
    오류 수정

--- backend/tourtogether/src/main/java/com/ssafy/tourtogether/api/service/BoardServiceImpl.java
+++ backend/tourtogether/src/main/java/com/ssafy/tourtogether/api/service/BoardServiceImpl.java
@@ -91,30 +91,6 @@ public class BoardServiceImpl implements BoardService {
 		}
 
 	}
-
-	private String makeSHA256(String boardRandom) {
-		try {
-			MessageDigest digest = MessageDigest.getInstance("SHA-256");
-			byte[] hash = digest.digest(boardRandom.getBytes("UTF-8"));
-			StringBuffer hexString = new StringBuffer();
-
-			for (int i = 0; i < hash.length; i++) {
-				String hex = Integer.toHexString(0xff & hash[i]);
-				if (hex.length() == 1)
-					hexString.append('0');
-				hexString.append(hex);
-			}
-		}
-
-	}
-
-
-			// 출력
-			return hexString.toString();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return null;
 	private String makeSHA256(String boardRandom) {
 		try {
 			MessageDigest digest = MessageDigest.getInstance("SHA-256");
