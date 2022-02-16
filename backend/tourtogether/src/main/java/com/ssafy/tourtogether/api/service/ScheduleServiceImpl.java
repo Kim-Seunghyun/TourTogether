a280b572c2ea4f4c7b9a442d4af91da512051b0f
minjunglee <3sally@naver.com>
2022-02-16 10:40:13 +0900

    :sparkles: Add add search schedule by board id

--- backend/tourtogether/src/main/java/com/ssafy/tourtogether/api/service/ScheduleServiceImpl.java
+++ backend/tourtogether/src/main/java/com/ssafy/tourtogether/api/service/ScheduleServiceImpl.java
@@ -1,19 +1,32 @@
 package com.ssafy.tourtogether.api.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ssafy.tourtogether.api.request.ScheduleSearchByBoardIdPostReq;
+import com.ssafy.tourtogether.db.entity.ScheduleDB;
+import com.ssafy.tourtogether.db.repository.ScheduleRepository;
 import com.ssafy.tourtogether.db.repository.ScheduleRepositorySupport;
 
 @Service("scheduleService")
 public class ScheduleServiceImpl implements ScheduleService {
 
 	@Autowired
+	ScheduleRepository scheduleRepository;
+	@Autowired
 	ScheduleRepositorySupport scheduleRepositorySupport;
-	
+
 	@Override
 	public void updateScheduleInScheduleRoom(String scheduleBoardId, String content) {
 		scheduleRepositorySupport.updateScheduleInScheduleRoom(scheduleBoardId, content);
-		
+
+	}
+
+	@Override
+	public List<ScheduleDB> searchByBoardId(ScheduleSearchByBoardIdPostReq scheduleSearchByBoardIdInfo) {
+		List<ScheduleDB> schedule = scheduleRepositorySupport.findByBoardId(scheduleSearchByBoardIdInfo);
+		return schedule;
 	}
 }
