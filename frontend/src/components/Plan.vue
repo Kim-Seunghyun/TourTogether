7ea8040b9f6aad3202b95c9595fcaeb329a16859
Gyum <ksh7669@naver.com>
2022-02-16 10:51:41 +0900

    Merge commit '68c029b00b6709de6c29b42b3cefc4d559c0aa4d' into develop
    # Conflicts:
    #	frontend/src/store/index.js

--- frontend/src/components/Plan.vue
+++ frontend/src/components/Plan.vue
@@ -179,7 +179,8 @@ export default {
     onMounted(() => {
       state.tourList = [];
       state.selectedIndex = 0;
-      state.id = window.location.pathname.split("/")[1];
+      state.id = window.location.pathname;
+      console.log(state.id);
       state.tourList[0] = { list: new Array(), index: 0 };
       // addDay();
       init();
@@ -244,6 +245,8 @@ export default {
     };
     const updateList = (response) => {
       state.tourList = JSON.parse(response.content);
+      console.log("updateList!!!!!!");
+      console.log(state.tourList);
       goEmit();
     };
     const init = () => {
@@ -256,10 +259,12 @@ export default {
         },
       })
         .then((response) => {
-          if(!response.data.schedule.scheduleContent){
+          if (!response.data.schedule.scheduleContent) {
             return;
           }
-          updateList(JSON.parse(response.data.schedule.scheduleContent));
+          let res = {};
+          res.content = response.data.schedule.scheduleContent;
+          updateList(res);
         })
         .catch((error) => {
           console.log(error);
