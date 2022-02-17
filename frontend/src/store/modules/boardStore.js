33226497b7adc363bdfaa5be7057d0fa5bc00c7a
hs <gudtjs0428@gmail.com>
2022-02-18 01:32:19 +0900

    :ambulance: Fix: add boardId to boardToDelete

--- frontend/src/store/modules/boardStore.js
+++ frontend/src/store/modules/boardStore.js
@@ -11,7 +11,7 @@ export const boardStore = {
     boardsDone: [],
     boardsLike: null,
     boardsLikeId: [],
-    boardToDelete: null,
+    boardToDelete: { boardId: 0},
     tourList: [],
     planFlag: true,
   },
