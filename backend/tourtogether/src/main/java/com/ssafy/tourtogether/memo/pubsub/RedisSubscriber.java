02552d6b93434532563c1cda0565b2ebcab645d9
오재우 <prkg7341@gmail.com>
2022-02-17 06:42:36 +0000

    :ambulance: Fix: redis backend hot fix
    Update RedisSubscriber.java

--- backend/tourtogether/src/main/java/com/ssafy/tourtogether/memo/pubsub/RedisSubscriber.java
+++ backend/tourtogether/src/main/java/com/ssafy/tourtogether/memo/pubsub/RedisSubscriber.java
@@ -2,6 +2,7 @@ package com.ssafy.tourtogether.memo.pubsub;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssafy.tourtogether.db.entity.MemoMessage;
+import com.ssafy.tourtogether.db.entity.ScheduleMessage;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,25 +17,31 @@ import org.springframework.stereotype.Service;
 @Service
 public class RedisSubscriber implements MessageListener {
 
-    private final ObjectMapper objectMapper;
-    private final RedisTemplate redisTemplate;
-    private final SimpMessageSendingOperations messagingTemplate;
+	private final ObjectMapper objectMapper;
+	private final RedisTemplate redisTemplate;
+	private final SimpMessageSendingOperations messagingTemplate;
 
-    /**
-     * Redis에서 메시지가 발행(publish)되면 대기하고 있던 onMessage가 해당 메시지를 받아 처리한다.
-     */
-    @Override
-    public void onMessage(Message message, byte[] pattern) {
-        try {
-            // redis에서 발행된 데이터를 받아 deserialize
-            String publishMessage = (String) redisTemplate.getStringSerializer().deserialize(message.getBody());
-            // MemoMessage 객채로 맵핑
-            MemoMessage roomMessage = objectMapper.readValue(publishMessage, MemoMessage.class);
-            // Websocket 구독자에게 채팅 메시지 Send
-            System.out.println("MemoSubscriber onMessage: "+roomMessage.toString());
-            messagingTemplate.convertAndSend("/api/sub/memo/"+roomMessage.getRoomId(), roomMessage);
-        } catch (Exception e) {
-            log.error(e.getMessage());
-        }
-    }
+	/**
+	 * Redis에서 메시지가 발행(publish)되면 대기하고 있던 onMessage가 해당 메시지를 받아 처리한다.
+	 */
+	@Override
+	public void onMessage(Message message, byte[] pattern) {
+		try {
+			// redis에서 발행된 데이터를 받아 deserialize
+			String publishMessage = (String) redisTemplate.getStringSerializer().deserialize(message.getBody());
+			// MemoMessage 객채로 맵핑
+			MemoMessage roomMessage = objectMapper.readValue(publishMessage, MemoMessage.class);
+			if (roomMessage.getDelta() == null) {
+				ScheduleMessage schduleMessage = objectMapper.readValue(publishMessage, ScheduleMessage.class);
+				System.out.println("SchedulSubscriber onMessage: " + schduleMessage.toString());
+				messagingTemplate.convertAndSend("/api/sub/schedule/" + schduleMessage.getRoomId(), schduleMessage);
+			} else {
+				// Websocket 구독자에게 채팅 메시지 Send
+				System.out.println("MemoSubscriber onMessage: " + roomMessage.toString());
+				messagingTemplate.convertAndSend("/api/sub/memo/" + roomMessage.getRoomId(), roomMessage);
+			}
+		} catch (Exception e) {
+			log.error(e.getMessage());
+		}
+	}
 }
